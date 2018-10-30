a60bd6df3f3a51915b79f5716ce5022e9e634936
Mayuresh2311 <44189586+Mayuresh2311@users.noreply.github.com>
2018-10-30 13:10:18 +0530

    Add files via upload

---
+++ lab_function1.py
@@ -0,0 +1,25 @@
+def ArmN(x): #function declaration
+ sum=0
+ t=x
+ while(t>0): #condition statement
+  d=t%10     #extracts last digit
+  sum+=d**3  #adds the cube
+  t=t//10    #removes remaining digits
+ if sum==x:  #checks the conditions 
+  return 'Armstrong number'
+ else:
+  return 'not armstrong'
+x=int(input()) #input statement
+print(ArmN(x)) #print statement
+
+	
+
+		
+
+
+
+
+
+
+
+
